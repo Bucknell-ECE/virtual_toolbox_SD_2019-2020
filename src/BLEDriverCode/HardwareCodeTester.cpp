75f975caead1f42372158c994e39fb5ce214095b
tav005 <60895513+tav005@users.noreply.github.com>
2020-02-27 16:03:07 -0500

    Update HardwareCodeTester.cpp

--- src/BLEDriverCode/HardwareCodeTester.cpp
+++ src/BLEDriverCode/HardwareCodeTester.cpp
@@ -14,4 +14,25 @@
  */
 int main(int argc, char *argv[]){
     ToolScanner ts = ToolScanner();
+}//
+// Created by MattTurconi on 2/5/2020.
+//
+
+#include "ToolScanner.h"
+#include <iostream>
+
+/**
+ * This is where you can run all your test code from.
+ * Define all your variables and try out the functions here.
+ *
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char *argv[]){
+    ToolScanner ts = ToolScanner();
+    vector<string> test = ts.scanForTools();
+    for (vector<string>::const_iterator i = test.begin(); i!=test.end(); ++i){
+        std::cout << (*i) << ' ';
+    }
 }
