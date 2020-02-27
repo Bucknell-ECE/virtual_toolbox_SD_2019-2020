55d6f254b2a429d5a80285db3e0d534d264d6819
ipt001 <60789314+ipt001@users.noreply.github.com>
2020-02-27 16:24:35 -0500

    Add limit switch to widget

--- src/FrontEnd/mainwidget.cpp
+++ src/FrontEnd/mainwidget.cpp
@@ -8,7 +8,7 @@ MissingWidget::MissingWidget(QWidget *parent) :
     QWidget(parent)
 {
    //foo = new QString();
-    button_ = new QPushButton(tr("Dismiss"));
+    button_ = new QPushButton(tr("Limit Switch"));
    textBrowser_ = new QTextBrowser();
    textBrowser_->resize(800,600);
    for ( const auto& i : model.Data()  )
@@ -21,6 +21,15 @@ MissingWidget::MissingWidget(QWidget *parent) :
    mainLayout->addWidget(textBrowser_,1,0);
    setLayout(mainLayout);
    setWindowTitle(tr("Missing Tools"));
+   
+   tl = new ToolScanner;
+   db_tools = new SQLiteDatabase("../dbFiles/ToolBox100.db", tl);
+   db_tools->addTool(to_string(22319), "Tag F5");
+    db_tools->addTool(to_string(52885), "Tag T2");
+    db_tools->addTool(to_string(140), "Tag T1");
+    db_tools->addTool(to_string(53644), "Tag T3");
+    db_tools->addTool(to_string(309), "Tag T4");
+    //db_tools->addTool(to_string(1), "Tag T10");
 
    connect(button_, SIGNAL(released()), this, SLOT(onButtonReleased()));
    connect(&process_, SIGNAL(readyReadStandardOutput()), this, SLOT(onCaptureProcessOutput()));
@@ -31,6 +40,8 @@ MissingWidget::~MissingWidget()
 {
     delete button_;
    delete textBrowser_;
+   delete tl;
+   delete db_tools;
 }
 
 // void MissingWidget::SetData(Missing_Model list){
@@ -59,6 +70,24 @@ void MissingWidget::onButtonReleased()
     // box.exec();
     // foo = box.textValue();
     // qDebug() << foo;
+    
+    //Gets the missing tools
+    vector<string> missing;
+    QStringList output;
+    
+    missing = db_tools->getMissingToolIDs();
+    //cout<<"Test " <<endl;
+    
+    //Converts into UI string object
+    for (int i = 0; i < missing.size(); i++){
+        QString next = QString::fromStdString(missing[i]);
+        //cout << missing[i] << "\n";
+        output.append(next);
+    }
+
+    //all that is needed for reloading data
+    SetData(output);
+    loadView();
 
     // Set up our process to write to stdout and run our command
     process_.setCurrentWriteChannel(QProcess::StandardOutput); // Set the write channel
